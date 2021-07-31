12baa98f4ea012f85bfef46ca3f2d7314d47a31a
gnmyt <germannewsmaker@gmail.com>
2021-07-31 22:28:10 +0200

    Added the `/action/reload` route

---
+++ src/main/java/de/gnmyt/mcdash/panel/routes/action/ReloadRoute.java
@@ -0,0 +1,26 @@
+package de.gnmyt.mcdash.panel.routes.action;
+
+import de.gnmyt.mcdash.api.handler.DefaultHandler;
+import de.gnmyt.mcdash.api.http.Request;
+import de.gnmyt.mcdash.api.http.ResponseController;
+import org.bukkit.Bukkit;
+
+public class ReloadRoute extends DefaultHandler {
+
+    @Override
+    public String path() {
+        return "reload";
+    }
+
+    /**
+     * Reloads the server
+     * @param request The request object from the HttpExchange
+     * @param response The Response controller from the HttpExchange
+     */
+    @Override
+    public void post(Request request, ResponseController response) throws Exception {
+        response.message("Action executed.");
+
+        runSync(Bukkit::reload);
+    }
+}
