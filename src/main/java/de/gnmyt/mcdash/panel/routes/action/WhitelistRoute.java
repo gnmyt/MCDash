8b2b83d2424a31452db4a600c89197e2572b5a03
gnmyt <germannewsmaker@gmail.com>
2021-07-31 22:31:22 +0200

    Added the `/action/whitelist` route

---
+++ src/main/java/de/gnmyt/mcdash/panel/routes/action/WhitelistRoute.java
@@ -0,0 +1,36 @@
+package de.gnmyt.mcdash.panel.routes.action;
+
+import de.gnmyt.mcdash.api.handler.DefaultHandler;
+import de.gnmyt.mcdash.api.http.Request;
+import de.gnmyt.mcdash.api.http.ResponseController;
+import org.bukkit.Bukkit;
+
+public class WhitelistRoute extends DefaultHandler {
+
+    @Override
+    public String path() {
+        return "whitelist";
+    }
+
+    /**
+     * Gets the current whitelist status
+     * @param request The request object from the HttpExchange
+     * @param response The Response controller from the HttpExchange
+     */
+    @Override
+    public void get(Request request, ResponseController response) throws Exception {
+        response.json("status="+Bukkit.hasWhitelist());
+    }
+
+    /**
+     * Updates the current whitelist status
+     * @param request The request object from the HttpExchange
+     * @param response The Response controller from the HttpExchange
+     */
+    @Override
+    public void patch(Request request, ResponseController response) throws Exception {
+        if (!isBooleanInBody("status")) return;
+        Bukkit.setWhitelist(getBooleanFromBody("status"));
+        response.message("Whitelist successfully " + (getBooleanFromBody("status") ? "enabled" : "disabled"));
+    }
+}
