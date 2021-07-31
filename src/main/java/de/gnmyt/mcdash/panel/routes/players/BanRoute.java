8dd87c764fe7ec20c525d2ad8852dd7ab19e6b56
gnmyt <germannewsmaker@gmail.com>
2021-07-31 22:52:52 +0200

    Added the `/players/banlist` route

---
+++ src/main/java/de/gnmyt/mcdash/panel/routes/players/BanRoute.java
@@ -0,0 +1,75 @@
+package de.gnmyt.mcdash.panel.routes.players;
+
+import de.gnmyt.mcdash.api.handler.DefaultHandler;
+import de.gnmyt.mcdash.api.http.ContentType;
+import de.gnmyt.mcdash.api.http.Request;
+import de.gnmyt.mcdash.api.http.ResponseController;
+import de.gnmyt.mcdash.api.json.ArrayBuilder;
+import org.bukkit.BanList;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+
+public class BanRoute extends DefaultHandler {
+
+    @Override
+    public String path() {
+        return "banlist";
+    }
+
+    /**
+     * Gets all banned players
+     * @param request The request object from the HttpExchange
+     * @param response The Response controller from the HttpExchange
+     */
+    @Override
+    public void get(Request request, ResponseController response) throws Exception {
+        ArrayBuilder builder = new ArrayBuilder();
+
+        for (OfflinePlayer player : Bukkit.getBannedPlayers()) {
+            builder.addNode()
+                    .add("uuid", player.getUniqueId().toString())
+                    .add("name", player.getName())
+                    .add("last_seen", player.getLastPlayed())
+                    .register();
+        }
+
+        response.type(ContentType.JSON).text(builder.toJSON());
+    }
+
+    /**
+     * Bans a player
+     * @param request The request object from the HttpExchange
+     * @param response The Response controller from the HttpExchange
+     */
+    @Override
+    public void put(Request request, ResponseController response) throws Exception {
+        if (!isStringInBody("username")) return;
+
+        String username = getStringFromBody("username");
+        String reason = getStringFromBody("reason") != null ? getStringFromBody("reason") : "";
+
+        Bukkit.getBanList(BanList.Type.NAME).addBan(username, reason, null, "MCDash");
+
+        if (Bukkit.getPlayer(username) != null)
+            runSync(() -> Bukkit.getPlayer(username).kickPlayer("You are banned from this server" + (reason.isEmpty() ? "." : ": " + reason)));
+
+        response.message("Successfully added the player to the ban list.");
+    }
+
+    /**
+     * Unbans a player
+     * @param request The request object from the HttpExchange
+     * @param response The Response controller from the HttpExchange
+     */
+    @Override
+    public void delete(Request request, ResponseController response) throws Exception {
+        if (!isStringInBody("username")) return;
+
+        String username = getStringFromBody("username");
+
+        Bukkit.getBanList(BanList.Type.NAME).pardon(username);
+
+        response.message("Successfully removed the player from the ban list.");
+    }
+
+}
