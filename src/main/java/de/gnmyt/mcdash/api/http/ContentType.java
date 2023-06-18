6751a666eba6b583cfcb4b71ceb983268e101915
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-18 17:45:35 +0200

    Added new content types to the ContentType.java

--- src/main/java/de/gnmyt/mcdash/api/http/ContentType.java
+++ src/main/java/de/gnmyt/mcdash/api/http/ContentType.java
@@ -8,27 +8,62 @@ public enum ContentType {
     /**
      * The default text content type
      */
-    TEXT("text/html"),
+    TEXT("text/html", "html"),
 
     /**
      * The json content type
      */
-    JSON("application/json"),
+    JSON("application/json", "json"),
 
     /**
      * The multipart content type
      * Used by the {@link de.gnmyt.mcdash.api.handler.MultipartHandler}
      */
-    MULTIPART("multipart/form-data");
+    MULTIPART("multipart/form-data", null),
+
+    /**
+     * The css content type
+     */
+    CSS("text/css", "css"),
+
+    /**
+     * The javascript content type
+     */
+    JAVASCRIPT("application/javascript", "js"),
+
+    /**
+     * The png content type
+     */
+    PNG("image/png", "png"),
+
+    /**
+     * The jpeg content type
+     */
+    JPEG("image/jpeg", "jpeg"),
+
+    /**
+     * The svg content type
+     */
+    SVG("image/svg+xml", "svg");
 
     private String type;
+    private String fileEnding;
 
     /**
      * The basic constructor of the {@link ContentType}
      * @param type The content type (header value)
      */
-    ContentType(String type) {
+    ContentType(String type, String fileEnding) {
         this.type = type;
+        this.fileEnding = fileEnding;
+    }
+
+    public static ContentType getContentType(String fileEnding) {
+        for (ContentType contentType : values()) {
+            if (contentType.getFileEnding() != null && fileEnding.endsWith(contentType.getFileEnding()))
+                return contentType;
+        }
+        return TEXT;
     }
 
     /**
@@ -37,5 +72,13 @@ public enum ContentType {
      */
     public String getType() {
         return type;
+    }
+
+    /**
+     * Gets the file ending of the content type
+     * @return the file ending of the content type
+     */
+    public String getFileEnding() {
+        return fileEnding;
     }
 }
