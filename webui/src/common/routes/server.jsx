4584d8bfd629880c4a10b99d3f46f5196aeb6f62
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-19 22:58:35 +0200

    Excluded the routes from the sidebar in the server.jsx

--- webui/src/common/routes/server.jsx
+++ webui/src/common/routes/server.jsx
@@ -2,47 +2,51 @@ import {Dashboard, Extension, Folder, Group, Public, Settings, Terminal} from "@
 import Overview from "@/states/Root/pages/Overview";
 import {BanListProvider} from "@/states/Root/pages/Players/components/PlayerTable/contexts/BanList";
 import Players from "@/states/Root/pages/Players";
+import Files from "@/states/Root/pages/Files";
 
-export default [
+export const routes = [
+    {path: "/", element: <Overview />},
+    {path: "/players", element: <BanListProvider><Players /></BanListProvider>},
+    {path: "/files/*", element: <Files />},
+    {path: "/console", element: <h2>Console</h2>},
+    {path: "/plugins", element: <h2>Plugins</h2>},
+    {path: "/worlds", element: <h2>Worlds</h2>,},
+    {path: "/configuration", element: <h2>Configuration</h2>,}
+]
+
+export const sidebar = [
     {
         path: "/",
-        element: <Overview />,
         icon: <Dashboard />,
         name: "Overview"
     },
     {
         path: "/players",
-        element: <BanListProvider><Players /></BanListProvider>,
         icon: <Group />,
         name: "Players"
     },
     {
         path: "/files",
-        element: <h2>Files</h2>,
         icon: <Folder />,
         name: "File Manager"
     },
     {
         path: "/console",
-        element: <h2>Console</h2>,
         icon: <Terminal />,
         name: "Console"
     },
     {
         path: "/plugins",
-        element: <h2>Plugins</h2>,
         icon: <Extension />,
         name: "Plugins"
     },
     {
         path: "/worlds",
-        element: <h2>Worlds</h2>,
         icon: <Public />,
         name: "Worlds"
     },
     {
         path: "/configuration",
-        element: <h2>Configuration</h2>,
         icon: <Settings />,
         name: "Configuration"
     }
