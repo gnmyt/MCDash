aede2dd1749b15bfc294c43bd18146f3d7c6113c
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-19 19:34:48 +0200

    Added the serverOnline state to the TokenContext.jsx

--- webui/src/common/contexts/Token/TokenContext.jsx
+++ webui/src/common/contexts/Token/TokenContext.jsx
@@ -1,23 +1,35 @@
 import {useState, createContext, useEffect} from "react";
 import {request} from "@/common/utils/RequestUtil.js";
+import {Alert, Snackbar} from "@mui/material";
 
 export const TokenContext = createContext({});
 
 export const TokenProvider = (props) => {
-
     const [tokenValid, setTokenValid] = useState(null);
+    const [serverOnline, setServerOnline] = useState(false);
 
     const checkToken = () => request("ping").then((r) => {
+        if (!r.ok && !(r.status === 400 || r.status === 401)) throw new Error("Server unavailable");
         setTokenValid(r.status === 200);
+        setServerOnline(true);
         return r.status === 200;
+    }).catch(() => {
+        setServerOnline(false);
     });
 
     useEffect(() => {
         checkToken();
+        const interval = setInterval(() => checkToken(), 10000);
+        return () => clearInterval(interval);
     }, []);
 
     return (
-        <TokenContext.Provider value={{tokenValid, checkToken}}>
+        <TokenContext.Provider value={{tokenValid, checkToken, serverOnline}}>
+            <Snackbar open={!serverOnline}>
+                <Alert severity="error" sx={{width: "100%"}}>
+                    The server is currently unavailable.
+                </Alert>
+            </Snackbar>
             {props.children}
         </TokenContext.Provider>
     )
