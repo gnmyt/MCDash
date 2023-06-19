2b873524dcaa6ee3a1cb851d60a63790b1b60d3b
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-19 23:02:51 +0200

    Created the FileView.jsx

---
+++ webui/src/states/Root/pages/Files/components/FileView/FileView.jsx
@@ -0,0 +1,30 @@
+import {Box, Button, Stack, Typography} from "@mui/material";
+import {Folder, InsertDriveFile} from "@mui/icons-material";
+import {convertSize} from "./utils/FileUtil.js";
+
+export const FileView = ({files, changeDirectory, click}) => {
+    return (
+        <div style={{display: "flex", flexWrap: "wrap", gap: 10, marginTop: 10, flexDirection: "column"}}>
+            {files.length === 0 &&
+                <Button variant="outlined" color="secondary" onClick={() => changeDirectory("..")}>Go
+                    back</Button>}
+            {files.map((file) => (
+                <Box key={file.name} display="flex" gap={1} padding={2}
+                     onClick={(event) => click(file, event)}
+                     backgroundColor={"background.darker"} borderRadius={5} style={{cursor: "pointer"}}
+                     onContextMenu={(e) => handleContextMenu(e, file)}>
+                    {file.is_folder && <Folder color="primary" style={{cursor: "pointer"}}/>}
+
+                    {!file.is_folder && <InsertDriveFile color="primary"/>}
+
+                    <Typography>{file.name}</Typography>
+
+                    <Stack direction="row" alignItems="center" gap={1} marginLeft="auto">
+                        <Typography>{convertSize(file.size)}</Typography>
+                        <Typography>{new Date(file.last_modified).toLocaleString()}</Typography>
+                    </Stack>
+                </Box>
+            ))}
+        </div>
+    );
+}
