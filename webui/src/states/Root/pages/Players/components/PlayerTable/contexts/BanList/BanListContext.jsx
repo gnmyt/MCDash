fa6e41866a34cb0543ee60adb2166d2e269f9a4b
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-19 19:38:38 +0200

    Created the BanListContext.jsx

---
+++ webui/src/states/Root/pages/Players/components/PlayerTable/contexts/BanList/BanListContext.jsx
@@ -0,0 +1,25 @@
+import {useState, useEffect, createContext} from "react";
+import {jsonRequest} from "@/common/utils/RequestUtil";
+
+export const BanListContext = createContext({});
+
+export const BanListProvider = (props) => {
+
+    const [bannedPlayers, setBannedPlayers] = useState([]);
+
+    const updatePlayers = () => {
+        jsonRequest("players/banlist").then(r => setBannedPlayers(r));
+    }
+
+    useEffect(() => {
+        updatePlayers();
+        const interval = setInterval(() => updatePlayers(), 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <BanListContext.Provider value={{bannedPlayers, updatePlayers}}>
+            {props.children}
+        </BanListContext.Provider>
+    );
+}
