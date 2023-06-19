189e927383801e79d38ae2b16aa3b442d6d01850
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-19 19:34:19 +0200

    Created the PlayerContext.jsx

---
+++ webui/src/common/contexts/Players/PlayerContext.jsx
@@ -0,0 +1,25 @@
+import {useState, useEffect, createContext} from "react";
+import {jsonRequest} from "@/common/utils/RequestUtil";
+
+export const PlayerContext = createContext({});
+
+export const PlayerProvider = (props) => {
+
+    const [players, setPlayers] = useState([]);
+
+    const updatePlayers = () => {
+        jsonRequest("players/online").then(r => setPlayers(r));
+    }
+
+    useEffect(() => {
+        updatePlayers();
+        const interval = setInterval(() => updatePlayers(), 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <PlayerContext.Provider value={{players, updatePlayers}}>
+            {props.children}
+        </PlayerContext.Provider>
+    );
+}
