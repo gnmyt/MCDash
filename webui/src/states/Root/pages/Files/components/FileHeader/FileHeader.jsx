3e7f46ad3b13fdab5dedcd45a18542cd37d729c8
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-19 23:02:26 +0200

    Created the FileHeader.jsx

---
+++ webui/src/states/Root/pages/Files/components/FileHeader/FileHeader.jsx
@@ -0,0 +1,21 @@
+import {Box, Chip, IconButton, Typography} from "@mui/material";
+import {Close, Save} from "@mui/icons-material";
+
+export const FileHeader = ({currentFile, directory, setDirectory, fileContentChanged, saveFile, setCurrentFile}) => {
+    return (
+        <Box sx={{display: "flex", alignItems: "center", justifyContent: "space-between", mt: 2, mb: 2}}>
+            <Typography variant="h5" fontWeight={500}>File Manager
+                {currentFile === null && directory.split("/").splice(0, directory.split("/").length - 1).map((dir, index) => (
+                    <Chip key={index} label={dir || "/"} color="secondary" style={{marginLeft: 5}}
+                          onClick={() => setDirectory(directory.substring(0, directory.indexOf(dir) + dir.length + 1))}/>
+                ))}
+            </Typography>
+
+            {currentFile !== null && fileContentChanged &&
+                <IconButton color="secondary" onClick={() => saveFile()}><Save /></IconButton>}
+
+            {currentFile !== null && !fileContentChanged &&
+                <IconButton color="secondary" onClick={() => setCurrentFile(null)}><Close /></IconButton>}
+        </Box>
+    );
+}
