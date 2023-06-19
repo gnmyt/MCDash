b76274e90310c45e5ab40831f41f514961f5170e
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-19 19:36:56 +0200

    Updated the Root.jsx

--- webui/src/states/Root/Root.jsx
+++ webui/src/states/Root/Root.jsx
@@ -1,21 +1,21 @@
 import {Navigate, Outlet} from "react-router-dom";
-import {TokenContext} from "@contexts/Token/index.js";
+import {TokenContext} from "@contexts/Token";
 import {useContext} from "react";
 import {Box, Toolbar} from "@mui/material";
 import Sidebar from "@/states/Root/components/Sidebar";
 import Header from "@/states/Root/components/Header";
 
 export const Root = () => {
-    const {tokenValid} = useContext(TokenContext);
+    const {tokenValid, serverOnline} = useContext(TokenContext);
 
     return (
         <div>
-            {tokenValid === false && <Navigate to="/login" />}
+            {tokenValid === false && serverOnline && <Navigate to="/login" />}
 
             <Box sx={{ display: 'flex' }}>
                 <Header />
                 <Sidebar />
-                <Box component="main" sx={{ pt: 1, pl: 2, pr: 1, pb: 1 }}>
+                <Box component="main" sx={{ pt: 1, pl: 2, pr: 1, pb: 1 }} style={{width: "80%"}}>
                     <Toolbar />
                     <Outlet />
                 </Box>
