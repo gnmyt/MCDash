50f8fc41622f57a69b768294e3371f425e1b8ba7
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-19 22:59:46 +0200

    Created the FileEditor.jsx

---
+++ webui/src/states/Root/pages/Files/components/FileEditor/FileEditor.jsx
@@ -0,0 +1,28 @@
+import CodeMirror from "@uiw/react-codemirror";
+import {atomone} from "@uiw/codemirror-theme-atomone";
+import {Box} from "@mui/material";
+import {useEffect} from "react";
+import {request} from "@/common/utils/RequestUtil.js";
+
+export const FileEditor = ({directory, currentFile, setContentChanged, fileContent, setFileContent}) => {
+
+    useEffect(() => {
+        if (currentFile === null) return setFileContent("");
+
+        request("filebrowser/file?path=." + directory + currentFile.name)
+            .then(async (data) => setFileContent(await data.text()));
+    }, [currentFile]);
+
+    const updateContent = (value) => {
+        setContentChanged(true);
+        setFileContent(value);
+    }
+
+    return (
+        <Box display="flex" flexDirection="column" gap={1} marginTop={2}><CodeMirror
+            value={fileContent}
+            onChange={updateContent}
+            theme={atomone}
+        /></Box>
+    )
+}
