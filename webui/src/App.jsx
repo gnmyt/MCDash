7a93719a8a9ec137a8538d988b5597df1281e54d
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-19 22:59:16 +0200

    Updated the App.jsx router

--- webui/src/App.jsx
+++ webui/src/App.jsx
@@ -3,7 +3,7 @@ import theme from "@/common/themes/dark.js";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Login from "@/states/Login";
 import Root from "@/states/Root";
-import routes from "@/common/routes/server.jsx";
+import {routes} from "@/common/routes/server.jsx";
 import {TokenProvider} from "@contexts/Token";
 import {PlayerProvider} from "@contexts/Players/index.js";
 
