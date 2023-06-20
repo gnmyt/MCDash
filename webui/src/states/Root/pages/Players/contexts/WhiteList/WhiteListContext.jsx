85c9ab20ba8d0c82987fffcf528ebd4d61d34c76
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-20 20:38:18 +0200

    Created the WhiteListContext.jsx context

---
+++ webui/src/states/Root/pages/Players/contexts/WhiteList/WhiteListContext.jsx
@@ -0,0 +1,32 @@
+import {useState, useEffect, createContext} from "react";
+import {jsonRequest, patchRequest} from "@/common/utils/RequestUtil";
+
+export const WhiteListContext = createContext({});
+
+export const WhiteListProvider = (props) => {
+
+    const [whitelistedPlayers, setWhitelistedPlayers] = useState([]);
+    const [whitelistActive, setWhitelistActive] = useState(false);
+
+    const updatePlayers = () => {
+        jsonRequest("players/whitelist").then(r => setWhitelistedPlayers(r));
+        jsonRequest("action/whitelist").then(r => setWhitelistActive(r.status));
+    }
+
+    useEffect(() => {
+        updatePlayers();
+        const interval = setInterval(() => updatePlayers(), 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    const switchWhitelist = () => {
+        patchRequest("action/whitelist", {status: !whitelistActive})
+            .then(() => updatePlayers());
+    }
+
+    return (
+        <WhiteListContext.Provider value={{whitelistedPlayers, whitelistActive, updatePlayers, switchWhitelist}}>
+            {props.children}
+        </WhiteListContext.Provider>
+    );
+}
