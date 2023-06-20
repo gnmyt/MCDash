c5c497de41bfe73be329eb39ef841037d56f0d31
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-20 20:35:17 +0200

    Added the server.jsx routes

--- webui/src/common/routes/server.jsx
+++ webui/src/common/routes/server.jsx
@@ -1,17 +1,19 @@
 import {Dashboard, Extension, Folder, Group, Public, Settings, Terminal} from "@mui/icons-material";
 import Overview from "@/states/Root/pages/Overview";
-import {BanListProvider} from "@/states/Root/pages/Players/components/PlayerTable/contexts/BanList";
+import {BanListProvider} from "@/states/Root/pages/Players/contexts/BanList";
 import Players from "@/states/Root/pages/Players";
 import Files from "@/states/Root/pages/Files";
+import { WhiteListProvider } from "@/states/Root/pages/Players/contexts/WhiteList";
+import Console from "@/states/Root/pages/Console";
 
 export const routes = [
     {path: "/", element: <Overview />},
-    {path: "/players", element: <BanListProvider><Players /></BanListProvider>},
+    {path: "/players", element: <BanListProvider><WhiteListProvider><Players /></WhiteListProvider></BanListProvider>},
     {path: "/files/*", element: <Files />},
-    {path: "/console", element: <h2>Console</h2>},
+    {path: "/console", element: <Console />},
     {path: "/plugins", element: <h2>Plugins</h2>},
-    {path: "/worlds", element: <h2>Worlds</h2>,},
-    {path: "/configuration", element: <h2>Configuration</h2>,}
+    {path: "/worlds", element: <h2>Worlds</h2>},
+    {path: "/configuration", element: <h2>Configuration</h2>}
 ]
 
 export const sidebar = [
