4967474a82603c80452316404bd69cdc7c891fd0
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-20 20:46:05 +0200

    Updated the Players.jsx page

--- webui/src/states/Root/pages/Players/Players.jsx
+++ webui/src/states/Root/pages/Players/Players.jsx
@@ -1,17 +1,26 @@
-import {Box, Button, ButtonGroup, Chip, Typography} from "@mui/material";
-import {useContext, useState} from "react";
-import PlayerActionDialog from "@/states/Root/pages/Players/components/PlayerActionDialog/index.js";
-import PlayerTable from "@/states/Root/pages/Players/components/PlayerTable/index.js";
-import {BanListContext} from "@/states/Root/pages/Players/components/PlayerTable/contexts/BanList/index.js";
-import {PlayerContext} from "@contexts/Players/index.js";
+import {Box, Button, ButtonGroup, Chip, IconButton, Stack, Tooltip, Typography} from "@mui/material";
+import React, {useContext, useState} from "react";
+import PlayerActionDialog from "@/states/Root/pages/Players/components/PlayerActionDialog";
+import PlayerTable from "@/states/Root/pages/Players/components/PlayerTable";
+import {BanListContext} from "./contexts/BanList";
+import {PlayerContext} from "@contexts/Players";
 import {deleteRequest} from "@/common/utils/RequestUtil.js";
 import BanListTable from "@/states/Root/pages/Players/components/BanListTable";
+import {WhiteListContext} from "@/states/Root/pages/Players/contexts/WhiteList";
+import WhiteListTable from "@/states/Root/pages/Players/components/WhiteListTable";
+import {PowerSettingsNew} from "@mui/icons-material";
+import {WhitelistAddDialog} from "@/states/Root/pages/Players/components/WhiteListAddDialog/WhitelistAddDialog.jsx";
 
 export const Players = () => {
     const {bannedPlayers, updatePlayers} = useContext(BanListContext);
+    const {whitelistActive, switchWhitelist, whitelistedPlayers, updatePlayers: updateWhitelisted} = useContext(WhiteListContext);
     const {players} = useContext(PlayerContext);
+
     const [selectedPlayers, setSelectedPlayers] = useState([]);
     const [selectedBannedPlayers, setSelectedBannedPlayers] = useState([]);
+    const [selectedWhitelistedPlayers, setSelectedWhitelistedPlayers] = useState([]);
+
+    const [whitelistDialogOpen, setWhitelistDialogOpen] = useState(false);
 
     const [dialogOpen, setDialogOpen] = useState(false);
     const [dialogAction, setDialogAction] = useState("");
@@ -20,6 +29,14 @@ export const Players = () => {
         if (selectedPlayers.length === 0) return;
         setDialogAction(action);
         setDialogOpen(true);
+    }
+
+    const removeWhitelistedPlayers = async () => {
+        for (const playerId of selectedWhitelistedPlayers) await deleteRequest(`players/whitelist/`,
+                {username: whitelistedPlayers.find((player) => player?.uuid === playerId).name});
+
+        setSelectedWhitelistedPlayers([]);
+        updateWhitelisted();
     }
 
     const unbanPlayers = async () => {
@@ -46,14 +63,43 @@ export const Players = () => {
 
             <PlayerTable setSelectedPlayers={setSelectedPlayers}/>
 
-            {bannedPlayers.length > 0 && <>
-                <Box sx={{display: "flex", alignItems: "center", justifyContent: "space-between", mt: 2, mb: 2}}>
-                    <Typography variant="h5" fontWeight={500}>Banned Players <Chip label={bannedPlayers.length} color="secondary"/></Typography>
-                    <Button color="primary" onClick={unbanPlayers} variant="contained">Unban</Button>
-                </Box>
+            <Stack spacing={3} direction="row" sx={{width: "100%", justifyContent: "space-between", mt: 2}} flexWrap="wrap">
+                <Stack spacing={2} direction="column" sx={{width: "48%"}}>
+                    <Box sx={{display: "flex", alignItems: "center", justifyContent: "space-between", mt: 2, mb: 2}}>
+                        <Typography variant="h5" fontWeight={500}>Whitelisted Players
+                            <Chip label={whitelistedPlayers.length} color="secondary"/></Typography>
 
-                <BanListTable setSelectedBannedPlayers={setSelectedBannedPlayers} />
-            </>}
+                        <Stack direction="row" spacing={2}>
+                            <Tooltip title={whitelistActive ? "Disable whitelist" : "Enable whitelist"}>
+                                <IconButton color={whitelistActive ? "success" : "error"} onClick={switchWhitelist}>
+                                    <PowerSettingsNew/>
+                                </IconButton>
+                            </Tooltip>
+                            <ButtonGroup>
+                                <Button color="primary" variant="contained" onClick={() =>
+                                    setWhitelistDialogOpen(true)}>Add</Button>
+                                <Button color="error" variant="contained"
+                                        onClick={removeWhitelistedPlayers}>Remove</Button>
+                            </ButtonGroup>
+                        </Stack>
+                    </Box>
+
+                    <WhitelistAddDialog open={whitelistDialogOpen} setOpen={setWhitelistDialogOpen}/>
+
+                    <WhiteListTable setSelectedWhitelistedPlayers={setSelectedWhitelistedPlayers}/>
+                </Stack>
+
+
+                <Stack spacing={2} direction="column" sx={{width: "48%"}}>
+                    <Box sx={{display: "flex", alignItems: "center", justifyContent: "space-between", mt: 2, mb: 2}}>
+                        <Typography variant="h5" fontWeight={500}>Banned Players <Chip label={bannedPlayers.length}
+                                                                                       color="secondary"/></Typography>
+                        <Button color="primary" onClick={unbanPlayers} variant="contained">Unban</Button>
+                    </Box>
+
+                    <BanListTable setSelectedBannedPlayers={setSelectedBannedPlayers}/>
+                </Stack>
+            </Stack>
         </>
     );
 
