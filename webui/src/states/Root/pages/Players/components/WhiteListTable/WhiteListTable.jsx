97edbc653905d8d625b9deaf8d85872631de5ca3
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-20 20:44:38 +0200

    Created the WhiteListTable.jsx

---
+++ webui/src/states/Root/pages/Players/components/WhiteListTable/WhiteListTable.jsx
@@ -0,0 +1,25 @@
+import {useContext} from "react";
+import {WhiteListContext} from "@/states/Root/pages/Players/contexts/WhiteList";
+import {DataGrid} from "@mui/x-data-grid";
+import columns from "./columns.jsx";
+
+export const WhiteListTable = ({setSelectedWhitelistedPlayers}) => {
+    const {whitelistedPlayers} = useContext(WhiteListContext);
+
+    return (
+        <div style={{height: 500}}>
+            <DataGrid
+                rows={whitelistedPlayers.map((player) => ({id: player?.uuid, ...player}))}
+                columns={columns}
+                initialState={{pagination: {paginationModel: {page: 0, pageSize: 10}}}}
+                pageSizeOptions={[10, 25, 50]}
+                checkboxSelection
+                disableColumnFilter={true}
+                disableColumnMenu={true}
+                onRowSelectionModelChange={(newSelection) => {
+                    setSelectedWhitelistedPlayers(newSelection);
+                }}
+            />
+        </div>
+    );
+}
