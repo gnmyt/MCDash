9f510ce9565aeff8c2b34c060b2416c7207008ac
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-20 20:44:48 +0200

    Created the WhiteListTable columns.jsx

---
+++ webui/src/states/Root/pages/Players/components/WhiteListTable/columns.jsx
@@ -0,0 +1,22 @@
+import {Typography} from "@mui/material";
+
+const columns = [
+    {
+        field: 'name', headerName: 'Username', minWidth: 150, flex: 1, renderCell: (params) => {
+            return (
+                <div style={{display: "flex", alignItems: "center"}}>
+                    <img src={`https://crafatar.com/avatars/${params.row.uuid}?size=25&overlay`} alt={params.row.name}
+                         style={{marginRight: 5}}/>
+                    <Typography>{params.row.name}</Typography>
+                </div>
+            )
+        }
+    },
+    {field: 'uuid', headerName: 'Player-ID', flex: 1, minWidth: 200},
+    {
+        field: 'last_seen', headerName: 'Last seen', flex: 1, minWidth: 100, renderCell: (params) =>
+            (<Typography>{new Date(params.row.last_seen).toLocaleString()}</Typography>)
+    }
+];
+
+export default columns;
