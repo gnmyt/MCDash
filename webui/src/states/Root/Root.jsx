ac7dc80c865f63fef28644a22a42e5651a27b0e4
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-22 21:19:50 +0200

    Optimized the Root.jsx for mobile devices

--- webui/src/states/Root/Root.jsx
+++ webui/src/states/Root/Root.jsx
@@ -1,25 +1,27 @@
 import {Navigate, Outlet} from "react-router-dom";
 import {TokenContext} from "@contexts/Token";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {Box, Toolbar} from "@mui/material";
 import Sidebar from "@/states/Root/components/Sidebar";
 import Header from "@/states/Root/components/Header";
 
 export const Root = () => {
+
+    const [mobileOpen, setMobileOpen] = useState(false);
     const {tokenValid, serverOnline} = useContext(TokenContext);
 
     return (
-        <div>
+        <>
             {tokenValid === false && serverOnline && <Navigate to="/login" />}
 
-            <Box sx={{ display: 'flex' }}>
-                <Header />
-                <Sidebar />
-                <Box component="main" sx={{ pt: 1, pl: 2, pr: 1, pb: 1 }} style={{width: "80%"}}>
+            <Box sx={{ display: 'flex', overflow: 'hidden'}}>
+                <Header mobileOpen={mobileOpen} toggleOpen={() => setMobileOpen(current => !current)} />
+                <Sidebar mobileOpen={mobileOpen} toggleOpen={() => setMobileOpen(current => !current)} />
+                <Box component="main" sx={{ flexGrow: 1, p: 3, ml: { sm: "240px"} }}>
                     <Toolbar />
                     <Outlet />
                 </Box>
             </Box>
-        </div>
+        </>
     )
 }
