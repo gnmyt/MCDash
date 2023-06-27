7963762d76a96476a0d61e3f362084f14b21a8b0
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-27 02:14:56 +0200

    Updated the docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   presets: [
     ['classic', ({
         docs: {sidebarPath: require.resolve('./sidebars.js'),
-            editUrl: 'https://github.com/gnmyt/MCDash/tree/master/docs/docs/'},
+            editUrl: 'https://github.com/gnmyt/MCDash/tree/master/docs/'},
         theme: {customCss: require.resolve('./src/css/custom.css')},
       }),
     ],
