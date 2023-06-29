34a15eaf366a97638736ed0dce2de821f42a1ddc
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-29 17:37:16 +0200

    Created the Game.jsx component (WIP)

---
+++ create/src/pages/NewServer/components/Game/Game.jsx
@@ -0,0 +1,9 @@
+import {Alert} from "@mui/material";
+
+export const Game = () => {
+return (
+        <>
+            <Alert severity="error">Still WIP! Please come back later.</Alert>
+        </>
+    )
+}
