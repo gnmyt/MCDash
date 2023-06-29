b1a2472fe8d6b0135afa12d58583fec4e695b907
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-29 16:14:25 +0200

    Imported the fonts, mui css & theme provider into the main.jsx

--- create/src/main.jsx
+++ create/src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import theme from "./common/themes/default.js";
+
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/700.css';
+import {CssBaseline, ThemeProvider} from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>,
 );
