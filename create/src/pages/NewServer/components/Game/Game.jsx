524e13149d3860892afcfe560c3e2d6fceecaaa5
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-30 21:30:19 +0200

    Updated the Game.jsx tab

--- create/src/pages/NewServer/components/Game/Game.jsx
+++ create/src/pages/NewServer/components/Game/Game.jsx
@@ -1,9 +1,27 @@
-import {Alert} from "@mui/material";
+import {
+    Checkbox,
+    FormControlLabel,
+    Link,
+    Stack,
+    TextField
+} from "@mui/material";
 
-export const Game = () => {
+const EULA_URL = "https://www.minecraft.net/eula";
+
+export const Game = ({eula, setEula, mcPort, setMcPort, panelPort, setPanelPort}) => {
 return (
         <>
-            <Alert severity="error">Still WIP! Please come back later.</Alert>
+            <Stack direction="column" justifyContent="space-between" spacing={2}>
+                <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+                    <TextField fullWidth label="Minecraft Port" variant="outlined" value={mcPort}
+                               onChange={(e) => setMcPort(e.target.value)} type="number"/>
+                    <TextField fullWidth label="Panel Port" variant="outlined" value={panelPort}
+                               onChange={(e) => setPanelPort(e.target.value)} type="number"/>
+                </Stack>
+
+                <FormControlLabel control={<Checkbox checked={eula} onChange={(e) => setEula(e.target.checked)}/>}
+                                  label={<>I accept the <Link href={EULA_URL} underline="hover" target="_blank">EULA</Link></>} />
+            </Stack>
         </>
     )
 }
