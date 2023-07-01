ce83b7f94ab0a5c60ca43720135aace2b75767da
Mathias Wagner <germannewsmaker@gmail.com>
2023-07-01 16:45:31 +0200

    Updated the versions in the Server.jsx

--- create/src/pages/NewServer/components/Server/Server.jsx
+++ create/src/pages/NewServer/components/Server/Server.jsx
@@ -1,17 +1,14 @@
 import {Autocomplete, FormControl, InputLabel, MenuItem, Select, Stack, TextField} from "@mui/material";
-
-const versions = [
-    "1.20.1", "1.19.4", "1.19.3", "1.19.2", "1.19.1",
-    "1.19", "1.18.2", "1.18.1", "1.18", "1.17.1", "1.17", "1.16.5",
-    "1.16.4", "1.16.3", "1.16.2", "1.16.1", "1.15.2",
-    "1.15.1", "1.15", "1.14.4", "1.14.3", "1.14.2", "1.14.1",
-    "1.14", "1.13.2", "1.13.1", "1.13", "1.12.2", "1.12.1",
-    "1.12", "1.11.2", "1.11.1", "1.11", "1.10",
-    "1.9.4", "1.9.2", "1.9", "1.8.8"
-]
+import {getVersions} from "./versions.js";
 
 export const Server = ({software, setSoftware, version, setVersion, instanceId,
                            setInstanceId, serverName, setServerName}) => {
+
+    const updateSoftware = (software) => {
+        if (!getVersions(software).includes(version)) setVersion(getVersions(software)[0]);
+        setSoftware(software);
+    }
+
     return (
         <>
             <Stack direction="column" justifyContent="space-between" spacing={2}>
@@ -19,14 +16,14 @@ export const Server = ({software, setSoftware, version, setVersion, instanceId,
                     <FormControl fullWidth>
                         <InputLabel id="software">Software</InputLabel>
                         <Select labelId="software" label="Software" value={software}
-                                onChange={(e) => setSoftware(e.target.value)}>
+                                onChange={(e) => updateSoftware(e.target.value)}>
                             <MenuItem value="spigot">Spigot</MenuItem>
                             <MenuItem value="paper">Paper</MenuItem>
                             <MenuItem value="purpur">Purpur</MenuItem>
                         </Select>
                     </FormControl>
 
-                    <Autocomplete options={versions} fullWidth value={version} onChange={(e, v) => setVersion(v)}
+                    <Autocomplete options={getVersions(software)} fullWidth value={version} onChange={(e, v) => setVersion(v)}
                                   renderInput={(params) => <TextField {...params} label="Version"/>}/>
                 </Stack>
 
