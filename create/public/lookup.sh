b8ce2c74c3c84dcc11ad6d1a9147ab2db7a61b82
Mathias Wagner <germannewsmaker@gmail.com>
2023-07-01 19:23:39 +0200

    Created the lookup.sh

---
+++ create/public/lookup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+find_minecraft_servers() {
+  local root_dir=$1
+
+  while IFS= read -r -d '' file; do
+    local server_path=$(dirname "$file")
+    local plugins_dir="$server_path/plugins"
+
+    if [[ -d "$plugins_dir" && ! -d "$plugins_dir/MinecraftDashboard" ]]; then
+      echo "[SERVER]$server_path[SERVER]"
+    fi
+  done < <(find "$root_dir" -name "bukkit.yml" -type f -print0)
+}
+
+find_minecraft_servers "/"
