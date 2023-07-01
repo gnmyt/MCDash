1af8cef556109db163bba15f935a5353287506fc
Mathias Wagner <germannewsmaker@gmail.com>
2023-07-01 19:49:05 +0200

    Created the plugin.sh

---
+++ create/public/plugin.sh
@@ -0,0 +1,18 @@
+function install_plugin() {
+  curl -L -o "$1/plugins/MCDash.jar" "https://api.spiget.org/v2/resources/110687/download"
+  echo "Plugin installed. Restart your server to complete the installation."
+}
+
+echo "Available Minecraft servers:"
+IFS=$'\n'
+
+servers=($(find "/" -name bukkit.yml -exec dirname {} \; 2>/dev/null))
+
+select server in "${servers[@]}"; do
+  if [[ -n $server ]]; then
+      install_plugin "$server"
+      break
+  else
+      echo "Invalid selection. Please try again."
+  fi
+done
