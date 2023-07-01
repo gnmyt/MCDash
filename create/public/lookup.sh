0d967b5af066415b7db346d9318e33bfe54eeea9
Mathias Wagner <germannewsmaker@gmail.com>
2023-07-01 21:05:25 +0200

    Updated the lookup.sh

--- create/public/lookup.sh
+++ create/public/lookup.sh
@@ -8,11 +8,11 @@ find_minecraft_servers() {
     local plugins_dir="$server_path/plugins"
 
     if [[ -d "$plugins_dir" && ! -d "$plugins_dir/MinecraftDashboard" ]]; then
-      echo "[SERVER]$server_path[SERVER]"
+      echo "MCDash | [SERVER]$server_path[SERVER]"
     fi
   done < <(find "$root_dir" -name "bukkit.yml" -type f -print0)
 }
 
 find_minecraft_servers "/"
 
-echo "[DONE]"
+echo "MCDash | [DONE]"
