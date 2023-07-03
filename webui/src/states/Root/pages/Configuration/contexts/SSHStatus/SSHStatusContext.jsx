2dafc51b12a33f36ba418317c6b73fe3f8b74e8d
Mathias Wagner <germannewsmaker@gmail.com>
2023-07-03 06:12:04 +0200

    Created the SSHStatusContext.jsx

---
+++ webui/src/states/Root/pages/Configuration/contexts/SSHStatus/SSHStatusContext.jsx
@@ -0,0 +1,29 @@
+import {createContext, useEffect, useState} from "react";
+import {jsonRequest} from "@/common/utils/RequestUtil.js";
+
+export const SSHStatusContext = createContext({});
+
+export const SSHStatusProvider = (props) => {
+
+    const [sshPort, setSshPort] = useState(22);
+    const [sshStatus, setSshStatus] = useState(false);
+
+    const updateStatus = () => {
+        jsonRequest("services/ssh").then((response) => {
+            setSshStatus(response.enabled);
+            setSshPort(response.port);
+        });
+    }
+
+    useEffect(() => {
+        updateStatus();
+        const interval = setInterval(() => updateStatus(), 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <SSHStatusContext.Provider value={{sshPort, sshStatus, updateStatus}}>
+            {props.children}
+        </SSHStatusContext.Provider>
+    );
+}
