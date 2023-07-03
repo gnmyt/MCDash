3666d849afcc16e1bcd4f68709fa0fc27403c46d
Mathias Wagner <germannewsmaker@gmail.com>
2023-07-03 18:33:22 +0200

    Updated the Configuration.jsx

--- webui/src/states/Root/pages/Configuration/Configuration.jsx
+++ webui/src/states/Root/pages/Configuration/Configuration.jsx
@@ -1,22 +1,21 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {PropertiesContext} from "@/states/Root/pages/Configuration/contexts/Properties";
 import {Box, Button, Stack, Switch, TextField, Typography} from "@mui/material";
 import ConfigurationItem from "@/states/Root/pages/Configuration/components/ConfigurationItem";
 import {Dns} from "@mui/icons-material";
 import {SSHStatusContext} from "@/states/Root/pages/Configuration/contexts/SSHStatus";
-import {patchRequest} from "@/common/utils/RequestUtil.js";
 
 export const Configuration = () => {
     const {properties, updateProperties} = useContext(PropertiesContext);
-    const {sshPort, setSshPort, sshStatus, updateStatus} = useContext(SSHStatusContext);
+    const {sshPort, sshStatus, updateSshPort, updateSshStatus} = useContext(SSHStatusContext);
+
+    const [sshPortState, setSshPortState] = useState(22);
+
+    useEffect(() => {
+        setSshPortState(sshPort);
+    }, [sshPort]);
 
     const retrieveUsername = () => atob(localStorage.getItem("token")).split(":")[0];
-
-    const updateSshStatus = () => patchRequest("services/ssh", {enabled: !sshStatus})
-        .then(() => updateStatus());
-
-    const updateSshPort = (event) => patchRequest("services/ssh", {port: event.target.value})
-        .then(() => updateStatus());
 
     return (
         <div>
@@ -34,8 +33,8 @@ export const Configuration = () => {
                     </Stack>
                     <Typography variant="body2">Manage your files and control the console from your computer.</Typography>
                     <Stack direction="row" spacing={1} alignItems="center" justifyContent="center" sx={{mt: 2}}>
-                        <TextField fullWidth label="SSH-Port" variant="outlined" value={sshPort} type="number" size="small"
-                                   onChange={(event) => setSshPort(event.target.value)} onBlur={updateSshPort}/>
+                        <TextField fullWidth label="SSH-Port" variant="outlined" value={sshPortState} type="number" size="small"
+                                   onChange={(event) => setSshPortState(event.target.value)} onBlur={updateSshPort}/>
                         <Button href={`sftp://${retrieveUsername()}@${window.location.hostname}:${sshPort}/`}
                                 variant="contained" disabled={!sshStatus} fullWidth >Connect</Button>
                     </Stack>
