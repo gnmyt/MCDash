4f6e344175ea1c764a92e82dc2f9217e41dcb948
Mathias Wagner <germannewsmaker@gmail.com>
2023-08-14 11:41:04 +0200

    Implemented the worlds route into the server.jsx

--- webui/src/common/routes/server.jsx
+++ webui/src/common/routes/server.jsx
@@ -1,4 +1,4 @@
-import {Dashboard, Extension, Folder, Group, Save, Settings, Terminal} from "@mui/icons-material";
+import {Dashboard, Extension, Folder, Group, Public, Save, Settings, Terminal} from "@mui/icons-material";
 import Overview from "@/states/Root/pages/Overview";
 import {BanListProvider} from "@/states/Root/pages/Players/contexts/BanList";
 import Players from "@/states/Root/pages/Players";
@@ -12,13 +12,16 @@ import Configuration from "@/states/Root/pages/Configuration";
 import {PropertiesProvider} from "@/states/Root/pages/Configuration/contexts/Properties";
 import Backups from "@/states/Root/pages/Backups";
 import {BackupProvider} from "@/states/Root/pages/Backups/contexts/Backups";
-import {SSHStatusProvider} from "@/states/Root/pages/Configuration/contexts/SSHStatus/index.js";
+import {SSHStatusProvider} from "@/states/Root/pages/Configuration/contexts/SSHStatus";
+import Worlds from "@/states/Root/pages/Worlds";
+import {WorldsProvider} from "@/states/Root/pages/Worlds/contexts/Worlds";
 
 export const routes = [
     {path: "/", element: <StatsProvider><Overview /></StatsProvider>},
     {path: "/players", element: <BanListProvider><WhiteListProvider><Players /></WhiteListProvider></BanListProvider>},
     {path: "/files/*", element: <Files />},
     {path: "/console", element: <Console />},
+    {path: "/worlds", element: <WorldsProvider><Worlds /></WorldsProvider>},
     {path: "/plugins", element: <PluginsProvider><Plugins /></PluginsProvider>},
     {path: "/backups", element: <BackupProvider><Backups/></BackupProvider>},
     {path: "/configuration", element: <PropertiesProvider><SSHStatusProvider><Configuration /></SSHStatusProvider></PropertiesProvider>}
@@ -44,6 +47,11 @@ export const sidebar = [
         path: "/console",
         icon: <Terminal />,
         name: "Console"
+    },
+    {
+        path: "/worlds",
+        icon: <Public />,
+        name: "Worlds"
     },
     {
         path: "/plugins",
