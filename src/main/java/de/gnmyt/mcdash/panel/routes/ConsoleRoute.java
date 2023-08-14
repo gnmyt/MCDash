f87fab98779fe2baeaa0b37efb7b5875853f60dc
Mathias Wagner <germannewsmaker@gmail.com>
2023-08-14 12:54:06 +0200

    The ConsoleRoute.java now uses the same logic as the MCCommand.java

--- src/main/java/de/gnmyt/mcdash/panel/routes/ConsoleRoute.java
+++ src/main/java/de/gnmyt/mcdash/panel/routes/ConsoleRoute.java
@@ -3,9 +3,12 @@ package de.gnmyt.mcdash.panel.routes;
 import de.gnmyt.mcdash.api.handler.DefaultHandler;
 import de.gnmyt.mcdash.api.http.Request;
 import de.gnmyt.mcdash.api.http.ResponseController;
+import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
@@ -23,21 +26,24 @@ public class ConsoleRoute extends DefaultHandler {
      */
     @Override
     public void get(Request request, ResponseController response) throws Exception {
-
         int startLine = getIntegerFromQuery(request, "startLine") != null ? getIntegerFromQuery(request, "startLine") : 1;
         int limit = getIntegerFromQuery(request, "limit") != null ? getIntegerFromQuery(request, "limit") : 500;
 
-        Object[] lines = Files.lines(Paths.get("logs//latest.log"))
-                .skip(startLine)
-                .limit(limit)
-                .toArray();
-
-        for (Object line : lines) {
-            if (!response.getResponse().getOutput().isEmpty()) response.writeToOutput("\n");
-            response.writeToOutput(line.toString());
+        Path path = Paths.get("logs/latest.log");
+        if (!Files.exists(path)) {
+            response.code(500).message("The log file does not exist");
+            return;
         }
 
-        response.send();
+        String[] lines = FileUtils.readFileToString(path.toFile(), StandardCharsets.UTF_8).split("\n");
+
+        StringBuilder log = new StringBuilder();
+        for (int i = startLine - 1; i < lines.length; i++) {
+            if (i >= limit) break;
+            log.append(lines[i]).append("\n");
+        }
+
+        response.text(log.toString());
     }
 
     /**
