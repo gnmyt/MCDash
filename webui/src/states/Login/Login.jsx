142299d0103952c8c6a94870e2d8645939506bf3
Mathias Wagner <germannewsmaker@gmail.com>
2023-08-15 21:25:17 +0200

    Translated the Login page

--- webui/src/states/Login/Login.jsx
+++ webui/src/states/Login/Login.jsx
@@ -3,6 +3,7 @@ import {TokenContext} from "@contexts/Token";
 import {Navigate} from "react-router-dom";
 import {Alert, Box, Button, Container, Grid, Stack, TextField, Typography} from "@mui/material";
 import {Buffer} from "buffer";
+import {t} from "i18next";
 
 export const Login = () => {
     const {tokenValid, checkToken} = useContext(TokenContext);
@@ -24,24 +25,23 @@ export const Login = () => {
                 <Grid container spacing={0} direction="column" justifyContent="center" style={{minHeight: "100vh"}}>
                     <Box sx={{
                         boxShadow: 5, borderRadius: 2, py: 4, display: "flex", flexDirection: "column",
-                        alignItems: "center", justifyContent: "center"
-                    }}>
+                        alignItems: "center", justifyContent: "center"}}>
                         <Stack direction="row" alignItems="center" gap={1}>
                             <img src="/assets/img/favicon.png" alt="MCDash" width="40px" height="40px" />
-                            <Typography variant="h5" noWrap>Sign In</Typography>
+                            <Typography variant="h5" noWrap>{t("login.sign_in")}</Typography>
                         </Stack>
                         {loginFailed && <Alert severity="error" sx={{mt: 1, width: "80%"}}>
-                            Login failed, check your credentials.
+                            {t("login.failed")}
                         </Alert>}
                         <Box component="form" onSubmit={login} sx={{mt: 1, width: "80%"}}>
-                            <TextField margin="normal" value={username} required fullWidth label="Ingame-Name"
+                            <TextField margin="normal" value={username} required fullWidth label={t("login.name")}
                                         autoFocus onChange={(e) => setUsername(e.target.value)}/>
 
-                            <TextField margin="normal" value={password} required fullWidth label="Password"
+                            <TextField margin="normal" value={password} required fullWidth label={t("login.password")}
                                         type="password" onChange={(e) => setPassword(e.target.value)}/>
 
                             <Button variant="contained" fullWidth sx={{mt: 3}} onClick={login} type="submit">
-                                Sign In
+                                {t("login.sign_in")}
                             </Button>
                         </Box>
                     </Box>
