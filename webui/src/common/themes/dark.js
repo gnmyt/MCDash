f1fe71d7ae201a09cebecd807016a1a1e2a5c163
Mathias Wagner <germannewsmaker@gmail.com>
2023-08-15 14:35:49 +0200

    Updated the dark.js theme

--- webui/src/common/themes/dark.js
+++ webui/src/common/themes/dark.js
@@ -32,10 +32,37 @@ const theme = createTheme({
                     backgroundColor: '#1A1722'
                 }
             }
-        }
+        },
+        MuiListItemButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: 15,
+                    margin: 4,
+                    marginLeft: 7,
+                    marginRight: 7
+                }
+            }
+        },
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+                        width: 5,
+                    },
+                    "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+                        borderRadius: 8,
+                        minHeight: 24,
+                        backgroundColor: "#34333e",
+                    },
+                    "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover": {
+                        backgroundColor: "#3B3A4A"
+                    }
+                },
+            },
+        },
     },
     shape: {
-        borderRadius: 4,
+        borderRadius: 8,
     }
 });
 
