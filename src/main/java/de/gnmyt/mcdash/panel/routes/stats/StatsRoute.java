01c5bf5e40a749736c74986042f77578a7c0e5d6
Mathias Wagner <germannewsmaker@gmail.com>
2023-08-17 18:35:32 +0200

    Added online_players & max_players to the StatsRoute.java

--- src/main/java/de/gnmyt/mcdash/panel/routes/stats/StatsRoute.java
+++ src/main/java/de/gnmyt/mcdash/panel/routes/stats/StatsRoute.java
@@ -5,6 +5,7 @@ import de.gnmyt.mcdash.api.controller.StatsController;
 import de.gnmyt.mcdash.api.handler.DefaultHandler;
 import de.gnmyt.mcdash.api.http.Request;
 import de.gnmyt.mcdash.api.http.ResponseController;
+import org.bukkit.Bukkit;
 
 public class StatsRoute extends DefaultHandler {
 
@@ -19,6 +20,7 @@ public class StatsRoute extends DefaultHandler {
     public void get(Request request, ResponseController response) {
         response.json("tps="+STATS.getTPS(), "processors="+STATS.getAvailableProcessors(),
                 "free_memory="+STATS.getFreeMemory(), "total_memory="+STATS.getTotalMemory(), "used_memory="+STATS.getUsedMemory(),
-                "free_space="+STATS.getFreeSpace(), "total_space="+STATS.getTotalSpace(), "used_space="+STATS.getUsedSpace());
+                "free_space="+STATS.getFreeSpace(), "total_space="+STATS.getTotalSpace(), "used_space="+STATS.getUsedSpace(),
+                "max_players="+ Bukkit.getMaxPlayers(), "online_players="+Bukkit.getOnlinePlayers().size());
     }
 }
