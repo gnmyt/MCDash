942aab1cb375441be22fb343cf19df2765e48360
Mathias Wagner <germannewsmaker@gmail.com>
2023-08-17 21:33:31 +0200

    Optimized the StatisticBox.jsx

--- webui/src/states/Root/pages/Overview/components/StatisticBox/StatisticBox.jsx
+++ webui/src/states/Root/pages/Overview/components/StatisticBox/StatisticBox.jsx
@@ -1,11 +1,23 @@
-import {Box, Typography} from "@mui/material";
+import {Box, Stack, Typography} from "@mui/material";
 
-export const StatisticBox = ({title, value}) => {
+export const StatisticBox = ({title, value, icon, color}) => {
+    const iconStyle = {borderRadius: 1.5, width: 50, height: 50,
+        justifyContent: "center", alignItems: "center", "& svg": {width: 30, height: 30},
+        "& img": {width: 50, height: 50, borderRadius: 1.5}};
+
     return (
-        <Box sx={{flex: 1, textAlign: "center", display: "flex", justifyContent: "center", flexDirection: "column"}} backgroundColor="background.darker" borderRadius={1}
+        <Box sx={{flex: 1, display: "flex", justifyContent: "center", flexDirection: "column"}}
+             backgroundColor="background.darker" borderRadius={1.5}
              padding={2}>
-            <Typography variant="h7" fontWeight={500}>{title}</Typography>
-            <Typography variant="h5" fontWeight={500} color="text.primary">{value}</Typography>
+            <Stack direction="row" alignItems="center" gap={2}>
+                <Stack sx={{...iconStyle, backgroundColor: color + ".main", color: color + ".contrastText"}}>
+                    {icon}
+                </Stack>
+                <Stack direction="column" justifyContent="center">
+                    <Typography variant="h7" fontWeight={600}>{title}</Typography>
+                    <Typography variant="body2" fontWeight={500}>{value}</Typography>
+                </Stack>
+            </Stack>
         </Box>
     )
 }
