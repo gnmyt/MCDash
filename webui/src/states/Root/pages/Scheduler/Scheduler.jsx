51c7dd74f16aba08ea0a6ecc5ea53a0836467e2d
Mathias Wagner <germannewsmaker@gmail.com>
2023-09-06 20:43:05 +0200

    Created the Scheduler.jsx page

---
+++ webui/src/states/Root/pages/Scheduler/Scheduler.jsx
@@ -0,0 +1,28 @@
+import {Button, Stack, Typography} from "@mui/material";
+import {t} from "i18next";
+import {Add} from "@mui/icons-material";
+import {useContext, useState} from "react";
+import {SchedulesContext} from "@/states/Root/pages/Scheduler/contexts/Schedules";
+import Schedule from "@/states/Root/pages/Scheduler/components/Schedule";
+import CreateScheduleDialog from "@/states/Root/pages/Scheduler/components/CreateScheduleDialog";
+
+export const Scheduler = () => {
+
+    const {schedules} = useContext(SchedulesContext);
+    const [dialogOpen, setDialogOpen] = useState(false);
+
+    return (
+        <Stack gap={2}>
+            <CreateScheduleDialog open={dialogOpen} setOpen={setDialogOpen}/>
+            <Stack direction="row" alignItems="center" justifyContent="space-between">
+                <Typography variant="h5" fontWeight={500}>{t("nav.schedules")}</Typography>
+                <Button variant="outlined" color="secondary" startIcon={<Add/>}
+                        onClick={() => setDialogOpen(true)}>{t("schedules.create")}</Button>
+            </Stack>
+
+            <Stack direction="column" gap={1}>
+                {schedules.map((schedule) => <Schedule key={schedule.name} {...schedule}/>)}
+            </Stack>
+        </Stack>
+    )
+}
