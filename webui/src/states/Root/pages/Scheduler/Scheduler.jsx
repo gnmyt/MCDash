ed60899e3fabcf3f8249f41ba48b30f39c89d3ad
Mathias Wagner <germannewsmaker@gmail.com>
2023-09-07 10:30:03 +0200

    Added "none found" to the Scheduler.jsx

--- webui/src/states/Root/pages/Scheduler/Scheduler.jsx
+++ webui/src/states/Root/pages/Scheduler/Scheduler.jsx
@@ -1,7 +1,7 @@
 import {Button, Stack, Typography} from "@mui/material";
 import {t} from "i18next";
 import {Add} from "@mui/icons-material";
-import {useContext, useState} from "react";
+import React, {useContext, useState} from "react";
 import {SchedulesContext} from "@/states/Root/pages/Scheduler/contexts/Schedules";
 import Schedule from "@/states/Root/pages/Scheduler/components/Schedule";
 import CreateScheduleDialog from "@/states/Root/pages/Scheduler/components/CreateScheduleDialog";
@@ -12,7 +12,7 @@ export const Scheduler = () => {
     const [dialogOpen, setDialogOpen] = useState(false);
 
     return (
-        <Stack gap={2}>
+        <Stack gap={2} sx={{mt: 2, mb: 2}}>
             <CreateScheduleDialog open={dialogOpen} setOpen={setDialogOpen}/>
             <Stack direction="row" alignItems="center" justifyContent="space-between">
                 <Typography variant="h5" fontWeight={500}>{t("nav.schedules")}</Typography>
@@ -22,6 +22,7 @@ export const Scheduler = () => {
 
             <Stack direction="column" gap={1}>
                 {schedules.map((schedule) => <Schedule key={schedule.name} {...schedule}/>)}
+                {schedules.length === 0 && <Typography textAlign="center">{t("backup.none_found")}</Typography>}
             </Stack>
         </Stack>
     )
