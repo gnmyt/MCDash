def2c295e952f90e9c9a205fb1a3e6e2b639b244
Mathias Wagner <germannewsmaker@gmail.com>
2023-09-08 09:58:21 +0200

    The FileEditor.jsx now shows the light editor theme if white mode is enabled

--- webui/src/states/Root/pages/Files/components/FileEditor/FileEditor.jsx
+++ webui/src/states/Root/pages/Files/components/FileEditor/FileEditor.jsx
@@ -1,12 +1,14 @@
 import CodeMirror from "@uiw/react-codemirror";
 import {dracula} from "@uiw/codemirror-theme-dracula";
 import {Box, Fab} from "@mui/material";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {patchRequest, request} from "@/common/utils/RequestUtil.js";
 import {Save} from "@mui/icons-material";
 import {t} from "i18next";
+import {SettingsContext} from "@contexts/Settings";
 
 export const FileEditor = ({directory, currentFile, setSnackbar}) => {
+    const {theme} = useContext(SettingsContext);
 
     const [fileContent, setFileContent] = useState("");
     const [fileContentChanged, setFileContentChanged] = useState(false);
@@ -40,7 +42,7 @@ export const FileEditor = ({directory, currentFile, setSnackbar}) => {
                                         onClick={saveFile}><Save/></Fab>}
             <Box sx={{borderRadius: 2, overflow: "hidden"}}>
                 <CodeMirror value={fileContent === null ? t("files.loading") : fileContent} onChange={updateContent}
-                            theme={dracula}/>
+                            theme={theme === "dark" ? dracula : "light"}/>
             </Box>
         </Box>
     )
