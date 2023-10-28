0df28860d44f177ebcd84684d62860342c6960cc
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-28 10:58:38 +0200

    Integrated fr, ja & pl in the ChangeLanguageDialog.jsx

--- webui/src/states/Root/components/Header/components/AccountMenu/components/ChangeLanguageDialog/ChangeLanguageDialog.jsx
+++ webui/src/states/Root/components/Header/components/AccountMenu/components/ChangeLanguageDialog/ChangeLanguageDialog.jsx
@@ -4,11 +4,23 @@ import {SettingsContext} from "@contexts/Settings";
 import GermanFlag from "@/common/assets/images/flags/de.webp";
 import EnglishFlag from "@/common/assets/images/flags/en.webp";
 import SpainFlag from "@/common/assets/images/flags/es.webp";
+import FrenchFlag from "@/common/assets/images/flags/fr.webp";
+import JapaneseFlag from "@/common/assets/images/flags/ja.webp";
+import PolishFlag from "@/common/assets/images/flags/pl.webp";
 import {t} from "i18next";
 
 export const ChangeLanguageDialog = ({open, setOpen}) => {
 
     const {language, updateLanguage} = useContext(SettingsContext);
+
+    const flags = [
+        {code: "en", flag: EnglishFlag, name: "English"},
+        {code: "de", flag: GermanFlag, name: "Deutsch"},
+        {code: "es", flag: SpainFlag, name: "Español"},
+        {code: "fr", flag: FrenchFlag, name: "Français"},
+        {code: "ja", flag: JapaneseFlag, name: "日本語"},
+        {code: "pl", flag: PolishFlag, name: "Polski"}
+    ]
 
     const [value, setValue] = useState(language);
 
@@ -22,24 +34,14 @@ export const ChangeLanguageDialog = ({open, setOpen}) => {
             <DialogTitle>{(t("header.update_language"))}</DialogTitle>
             <DialogContent>
                 <Select fullWidth value={value} onChange={(e) => setValue(e.target.value)} variant="outlined">
-                    <MenuItem value="en">
-                        <Stack direction={"row"} gap={1} alignItems={"center"}>
-                            <Box component="img" src={EnglishFlag} height={12} alt="English" borderRadius={0.2}/>
-                            English
-                        </Stack>
-                    </MenuItem>
-                    <MenuItem value="de">
-                        <Stack direction={"row"} gap={1} alignItems={"center"}>
-                            <Box component="img" src={GermanFlag} height={12} alt="English" borderRadius={0.2}/>
-                            Deutsch
-                        </Stack>
-                    </MenuItem>
-                    <MenuItem value="es">
-                        <Stack direction={"row"} gap={1} alignItems={"center"}>
-                            <Box component="img" src={SpainFlag} height={12} alt="Spanish" borderRadius={0.2}/>
-                            Español
-                        </Stack>
-                    </MenuItem>
+                    {flags.map((flag) => (
+                        <MenuItem key={flag.code} value={flag.code}>
+                            <Stack direction={"row"} gap={1} alignItems={"center"}>
+                                <Box component="img" src={flag.flag} height={12} alt={flag.name} borderRadius={0.2}/>
+                                {flag.name}
+                            </Stack>
+                        </MenuItem>
+                    ))}
                 </Select>
             </DialogContent>
             <DialogActions>
