d790f15a7fa3eb00897bb49727522712f53b48a7
Mathias Wagner <germannewsmaker@gmail.com>
2024-12-30 00:17:04 +0100

    Add JSONRequest methods

--- modules/api/src/main/java/de/gnm/mcdash/api/http/JSONRequest.java
+++ modules/api/src/main/java/de/gnm/mcdash/api/http/JSONRequest.java
@@ -13,9 +13,10 @@ public class JSONRequest extends Request {
 
     /**
      * Constructor for JSONRequest
-     * @param address The IP address of the client
-     * @param headers The headers of the request
-     * @param parameters The parameters of the request
+     *
+     * @param address     The IP address of the client
+     * @param headers     The headers of the request
+     * @param parameters  The parameters of the request
      * @param requestJson The JSON body of the request
      */
     public JSONRequest(InetAddress address, HeaderMap headers, Map<String, String> parameters, JsonNode requestJson) {
@@ -25,6 +26,7 @@ public class JSONRequest extends Request {
 
     /**
      * Gets the value of a key in the JSON body
+     *
      * @param key The key to get the value of
      * @return The value of the key as a string
      */
@@ -33,7 +35,46 @@ public class JSONRequest extends Request {
     }
 
     /**
+     * Checks if the JSON body has a key
+     *
+     * @param keys The keys to check for
+     * @return Whether the JSON body has the key
+     */
+    public boolean has(String... keys) {
+        for (String key : keys) {
+            if (!requestJson.has(key)) return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if the JSON body has a key and the value is not null
+     *
+     * @param keys The keys to check for
+     * @return Whether the JSON body has the key and the value is not null
+     */
+    public boolean hasNonNull(String... keys) {
+        for (String key : keys) {
+            if (requestJson.hasNonNull(key) && !requestJson.get(key).asText().isEmpty()) return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if the JSON body has a key
+     *
+     * @param keys The keys to check for
+     */
+    public void checkFor(String... keys) {
+        for (String key : keys) {
+            if (!requestJson.has(key))
+                throw new IllegalArgumentException("The key '" + key + "' is missing in the JSON body");
+        }
+    }
+
+    /**
      * Gets the value of a key in the JSON body as an integer
+     *
      * @param key The key to get the value of
      * @return The value of the key as an integer
      */
@@ -43,6 +84,7 @@ public class JSONRequest extends Request {
 
     /**
      * Gets the value of a key in the JSON body as a boolean
+     *
      * @param key The key to get the value of
      * @return The value of the key as a boolean
      */
@@ -52,6 +94,7 @@ public class JSONRequest extends Request {
 
     /**
      * Gets the value of a key in the JSON body as a double
+     *
      * @param key The key to get the value of
      * @return The value of the key as a double
      */
@@ -61,6 +104,7 @@ public class JSONRequest extends Request {
 
     /**
      * Gets the value of a key in the JSON body as a JsonNode
+     *
      * @param key The key to get the value of
      * @return The value of the key as a JsonNode
      */
@@ -70,6 +114,7 @@ public class JSONRequest extends Request {
 
     /**
      * Gets the value of a key in the JSON body as an ArrayNode
+     *
      * @param key The key to get the value of
      * @return The value of the key as an ArrayNode
      */
