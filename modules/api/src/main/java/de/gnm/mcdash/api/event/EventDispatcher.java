c15ae7b6a67239e9f97908ecd21bbd91dc797bf5
Mathias Wagner <germannewsmaker@gmail.com>
2024-12-31 22:27:13 +0100

    Create EventDispatcher.java

---
+++ modules/api/src/main/java/de/gnm/mcdash/api/event/EventDispatcher.java
@@ -0,0 +1,41 @@
+package de.gnm.mcdash.api.event;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class EventDispatcher {
+
+    private final Map<Class<? extends BaseEvent>, List<Consumer<? extends BaseEvent>>> listeners = new HashMap<>();
+
+    /**
+     * Registers a listener for a specific event type.
+     *
+     * @param eventType The class of the event to listen for
+     * @param listener  The listener to handle the event
+     * @param <T>       The type of the event
+     */
+    public <T extends BaseEvent> void registerListener(Class<T> eventType, Consumer<T> listener) {
+        listeners.computeIfAbsent(eventType, k -> new ArrayList<>()).add(listener);
+        System.out.println("Registered listener for " + eventType.getSimpleName());
+    }
+
+    /**
+     * Dispatches an event to all its registered listeners.
+     *
+     * @param event The event instance to dispatch
+     */
+    @SuppressWarnings("unchecked")
+    public void dispatch(BaseEvent event) {
+        Class<? extends BaseEvent> eventType = event.getClass();
+        List<Consumer<? extends BaseEvent>> eventListeners = listeners.get(eventType);
+
+        if (eventListeners != null) {
+            for (Consumer<? extends BaseEvent> listener : eventListeners) {
+                ((Consumer<BaseEvent>) listener).accept(event);
+            }
+        }
+    }
+}
