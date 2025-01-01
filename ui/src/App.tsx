4d5894ec1d4dc260d90a0d1c45571ccc8bbed52d
Mathias Wagner <germannewsmaker@gmail.com>
2025-01-01 22:38:36 +0100

    Add Toaster to App.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,17 +4,18 @@ import i18n from "./i18n.ts";
 import {useState} from "react";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-import {routes} from "@/states/Root/routes.tsx";
+import {getRoutes} from "@/states/Root/routes.tsx";
 import {ThemeProvider} from "@/components/theme-provider.tsx";
 import Root from "@/states/Root/Root.tsx";
 import {ServerInfoProvider} from "@/contexts/ServerInfoContext.tsx";
+import {Toaster} from "@/components/ui/toaster.tsx";
 
 const App = () => {
     const [translationsLoaded, setTranslationsLoaded] = useState(false);
 
     const router = createBrowserRouter([
         {path: "/login", element: <Login />},
-        {path: "/", element: <Root />, children: routes}
+        {path: "/", element: <Root />, children: getRoutes()}
     ]);
 
     i18n.on("initialized", () => setTranslationsLoaded(true));
@@ -24,6 +25,7 @@ const App = () => {
     return (
         <ThemeProvider defaultTheme="dark" storageKey="theme">
             <ServerInfoProvider>
+                <Toaster />
                 <RouterProvider router={router}/>
             </ServerInfoProvider>
         </ThemeProvider>
