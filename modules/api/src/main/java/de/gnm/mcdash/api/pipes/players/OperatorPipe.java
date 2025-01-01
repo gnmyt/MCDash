7aed6947acf9729d1945ef8c7f0292fd325184e7
Mathias Wagner <germannewsmaker@gmail.com>
2025-01-01 01:41:24 +0100

    Add comments to pipes

--- modules/api/src/main/java/de/gnm/mcdash/api/pipes/players/OperatorPipe.java
+++ modules/api/src/main/java/de/gnm/mcdash/api/pipes/players/OperatorPipe.java
@@ -6,7 +6,24 @@ import de.gnm.mcdash.api.pipes.BasePipe;
 import java.util.ArrayList;
 
 public interface OperatorPipe extends BasePipe {
+    /**
+     * Returns a list of all operators
+     *
+     * @return a list of all operators
+     */
     ArrayList<OfflinePlayer> getOperators();
+
+    /**
+     * Sets a player as operator
+     *
+     * @param playerName the name of the player
+     */
     void setOp(String playerName);
+
+    /**
+     * Removes a player from the operator list
+     *
+     * @param playerName the name of the player
+     */
     void deOp(String playerName);
 }
--- modules/api/src/main/java/de/gnm/mcdash/api/pipes/players/WhitelistPipe.java
+++ modules/api/src/main/java/de/gnm/mcdash/api/pipes/players/WhitelistPipe.java
@@ -7,11 +7,39 @@ import java.util.ArrayList;
 
 public interface WhitelistPipe extends BasePipe {
 
+    /**
+     * Sets the status of the whitelist
+     *
+     * @param status the status of the whitelist
+     */
     void setStatus(boolean status);
+
+    /**
+     * Gets the status of the whitelist
+     *
+     * @return the status of the whitelist
+     */
     boolean getStatus();
 
+    /**
+     * Gets all whitelisted players
+     *
+     * @return all whitelisted players
+     */
     ArrayList<OfflinePlayer> getWhitelistedPlayers();
+
+    /**
+     * Adds a player to the whitelist
+     *
+     * @param playerName the name of the player
+     */
     void addPlayer(String playerName);
+
+    /**
+     * Removes a player from the whitelist
+     *
+     * @param playerName the name of the player
+     */
     void removePlayer(String playerName);
 
 }
