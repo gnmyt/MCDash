5ae90551725e782a627f22937d8fb2dcb685be1f
Mathias Wagner <germannewsmaker@gmail.com>
2025-01-02 02:46:02 +0100

    Add new languages to i18n.ts

--- ui/src/i18n.ts
+++ ui/src/i18n.ts
@@ -1,25 +1,30 @@
 import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 
-if (localStorage.getItem('language') === null)
-    localStorage.setItem('language', navigator.language.split('-')[0]);
+if (localStorage.getItem("language") === null)
+    localStorage.setItem("language", navigator.language.split("-")[0]);
 
-export const languages = {
-    de: "Deutsch",
-    en: "English"
-}
+
+export const languages = [
+    {code: "de", name: "Deutsch", imageCode: "de"},
+    {code: "en", name: "English", imageCode: "gb"},
+    {code: "es", name: "Español", imageCode: "es"},
+    {code: "fr", name: "Français", imageCode: "fr"},
+    {code: "ja", name: "日本語", imageCode: "jp"},
+    {code: "pl", name: "Polski", imageCode: "pl"}
+]
 
 i18n.use(initReactI18next).use(LanguageDetector).use(HttpApi).init({
-    supportedLngs: Object.keys(languages),
-    fallbackLng: 'en',
+    supportedLngs: languages.map((lang) => lang.code),
+    fallbackLng: "en",
     backend: {
-        loadPath: '/assets/locales/{{lng}}.json'
+        loadPath: "/assets/locales/{{lng}}.json"
     },
     detection: {
-        order: ['localStorage'],
-        lookupLocalStorage: 'language'
+        order: ["localStorage"],
+        lookupLocalStorage: "language"
     }
 });
 
