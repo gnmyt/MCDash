bce5dcd94c30b21989c1715834acec266dea8f1a
Mathias Wagner <germannewsmaker@gmail.com>
2025-01-11 22:12:05 +0100

    Add websocket support to vite.config.ts

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,17 +1,21 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import {defineConfig} from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [react()],
     server: {
         proxy: {
-            "/api": "http://localhost:7867"
-        }
+            "/api": {
+                target: "http://localhost:7867",
+                changeOrigin: true,
+                ws: true,
+            },
+        },
     },
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src")
+            "@": path.resolve(__dirname, "./src"),
         },
     },
-})
+});
