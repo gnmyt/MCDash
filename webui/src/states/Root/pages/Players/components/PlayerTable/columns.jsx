86120e979ecec051871f6b826910b85d29824140
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2025-12-11 11:52:39 +0100

    Update player avatar image source in PlayerTable

--- webui/src/states/Root/pages/Players/components/PlayerTable/columns.jsx
+++ webui/src/states/Root/pages/Players/components/PlayerTable/columns.jsx
@@ -11,7 +11,7 @@ const columns = ({setOP, teleportPlayer}) => [
         field: 'name', headerName: t("players.username"), minWidth: 200, flex: 1, renderCell: (params) => {
             return (
                 <div style={{display: "flex", alignItems: "center"}}>
-                    <img src={`https://crafatar.com/avatars/${params.row.uuid}?size=25&overlay`} alt={params.row.name}
+                    <img src={`https://mc-heads.net/avatar/${params.row.uuid}/25`} alt={params.row.name}
                          style={{marginRight: 5}}/>
                     <Typography>{params.row.name}</Typography>
                 </div>
