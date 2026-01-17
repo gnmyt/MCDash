84cb65b6c918f64ae3d6f1a3e097bc72de6d1d4b
Mathias Wagner <germannewsmaker@gmail.com>
2026-01-17 21:40:27 +0100

    Add site metadata and configure Nitro prerendering for docs

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -3,4 +3,33 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
   compatibilityDate: '2024-07-06',
+
+  site: {
+    url: 'https://mcdash.gnm.dev',
+  },
+
+  ogImage: {
+    enabled: false,
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: [
+        '/',
+        '/getting-started/introduction',
+        '/getting-started/reverse-proxy',
+        '/features/overview',
+        '/features/players',
+        '/features/file_manager',
+        '/features/console',
+        '/features/worlds',
+        '/features/plugins',
+        '/features/backups',
+        '/features/schedules',
+        '/features/configuration',
+      ],
+      failOnError: false,
+    },
+  },
 });
